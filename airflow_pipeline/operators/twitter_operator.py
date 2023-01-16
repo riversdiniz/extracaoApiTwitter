092db4ca8cb2021e67f6a714a87896e4f4421655
River Diniz <river-s-diniz@hotmail.com>
2023-01-16 09:37:24 -0400

    Estruturação para um datalake

--- airflow_pipeline/operators/twitter_operator.py
+++ airflow_pipeline/operators/twitter_operator.py
@@ -5,21 +5,29 @@ from airflow.models import BaseOperator, DAG, TaskInstance
 from hook.twitter_hook import TwitterHook
 import json
 from datetime import datetime, timedelta
+from os.path import join
+from pathlib import Path
 
 class TwitterOperator(BaseOperator):
 
-    def __init__(self, end_time, start_time, query, **kwargs):
+    def __init__(self,file_path, end_time, start_time, query, **kwargs):
         self.end_time = end_time
         self.start_time = start_time
         self.query = query
+        self.file_path = file_path
         super().__init__(**kwargs)
+
+    def create_parent_folder(self):
+        (Path(self.file_path).parent).mkdir(parents=True, exist_ok=True)
 
     def execute(self, context):
         end_time = self.end_time
         start_time = self.start_time
         query = self.query
 
-        with open("extract_twitter.json", "w") as output_file:
+        self.create_parent_folder()
+
+        with open(self.file_path, "w") as output_file:
             for pgnas in TwitterHook(end_time, start_time, query).run():
                 json.dump(pgnas, output_file, ensure_ascii=False)
                 output_file.write("\n")
@@ -34,7 +42,10 @@ if __name__ == "__main__":
     query = "datascience"
 
     with DAG(dag_id = "TwitterTest", start_date=datetime.now()) as dag:
-        to = TwitterOperator(query=query, start_time=start_time, end_time=end_time, task_id="test_run")
+        to = TwitterOperator(file_path=join("datalake/twitter_datascience",
+                                            f"extract_date={datetime.now().date()}",
+                                            f"datascience_{datetime.now().date().strftime('%Y%m%d')}.json"),
+                                            query=query, start_time=start_time, end_time=end_time, task_id="test_run")
         ti = TaskInstance(task=to)
         to.execute(ti.task_id)
         
